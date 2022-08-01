4a708fa13ca25bedd6371d60c458e74b5d0c0995
ObedAmpah7 <72440542+ObedAmpah7@users.noreply.github.com>
2022-08-01 14:28:57 +0000

    inti

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,69 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+    >
+      <h1>About</h1>
+    </transition>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
   </div>
 </template>
 
 <script>
-
+import gsap from 'gsap'
 export default {
   setup() {
+    const beforeEnter = (el) => {
+      el.style.transform = 'translateY(-60px)'
+      el.style.opacity = 0
+    };
+    const enter = (el, done) => {
+      gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      })
+    };
+    const afterEnter = () => {
+      console.log('after leave');
+    };
 
-  }
-}
+    // const beforeLeave = () => {};
+
+    // const leave = () => {};
+
+    // const afterLeave = () => {};
+
+    return { beforeEnter, enter, afterEnter};
+  },
+};
 </script>
 
 <style>
-  .about {
-    max-width: 600px;
-    margin: 20px auto;
-  }
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
 </style>
